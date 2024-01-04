66de7c0ac7083c1bf1551510ffbe4d29b8863bf9
artificialair <jaybaker964@gmail.com>
2024-01-04 18:54:10 -0500

    made answers async for readability purposes

--- index.js
+++ index.js
@@ -1,13 +1,83 @@
 // TODO: Include packages needed for this application
+const fs = require("fs")
+const inquirer = require("inquirer")
+const generateMarkdown = require("./utils/generateMarkdown.js")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+const questions = [
+    {
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        name: "description",
+        message: "Provide a short description explaining the what, why, and how of your project:"
+    },
+    {
+        name: "usage",
+        message: "Provide instructions and examples for use:"
+    },
+    {
+        type: "rawlist",
+        name: "license",
+        message: "What type of License do you use?",
+        choices: [
+            "Apache License 2.0",
+            "MIT License",
+            "GNU GPLv3 License",
+            "Mozilla Public License 2.0",
+            "No License"
+        ]
+    },
+    {
+        name: "contribute",
+        message: "Provide instructions for how other developers can contribute to your project:"
+    },
+    {
+        name: "tests",
+        message: "Provide examples on how to run tests written for your application:"
+    },
+    {
+        name: "github",
+        message: "What is your GitHub username?"
+    },
+    {
+        name: "email",
+        message: "What is your e-mail address?"
+    }
+];
+
+async function askQuestions() {
+    const answers = await inquirer.prompt(questions)
+    return answers;
+
+    /*
+    // This is the method we were taught to use, I used async because it makes the code a little easier to follow.
+
+    inquirer.prompt(questions)
+    .then(answers => {
+        return answers
+    });
+    */
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    if (!fs.existsSync("./output")) {
+        fs.mkdirSync("./output");
+    }
+
+    fs.writeFileSync("./output/" + fileName, data);
+    console.log("README has been created at ./output/" + fileName);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    const answers = await askQuestions();
+    const readme = generateMarkdown(answers);
+    writeToFile("README.md", readme);
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,64 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const shortenedLicense = license.replace(/ .*/,'');  // Uses a regex to get the first word of the string
+  return `
+  ![license](https://img.shields.io/badge/License-${shortenedLicense}-blue)
+  `
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "No License") return "";
+  return `
+  ## License
+  ${renderLicenseBadge(license)}
+  This repository is protected under the ${license}.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
-`;
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents
+  
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contribute](#contribute)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Usage
+  
+  ${data.usage}
+  ${renderLicenseSection(data.license)}
+  
+  ## How to Contribute
+  
+  ${data.contribute}
+
+  ## Tests
+  
+  ${data.tests}
+
+  ## Questions
+
+  If you have any questions, feel free to open an issue on this repository, or contact me.
+  [GitHub](https://github.com/${data.github})
+  E-mail: ${data.email}
+  `;
 }
 
 module.exports = generateMarkdown;
